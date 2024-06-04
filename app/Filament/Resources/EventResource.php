33c17c02bc2eb8732f76d1c9b159431a0edea2e7
Ivan on Linux <admin@codprez.com>
2024-06-04 12:08:33 +0300

    fixed events creation

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -7,6 +7,8 @@ use App\Models\Event;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextArea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TimePicker;
@@ -15,65 +17,70 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 
-class EventResource extends Resource {
-	protected static ?string $model = Event::class;
+class EventResource extends Resource
+{
+    protected static ?string $model = Event::class;
 
-	protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-	protected static ?string $navigationGroup = 'Modules';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Modules';
 
-	public static function form(Form $form): Form {
-		return $form
-			->schema([
-				TextInput::make('name'),
-				TextInput::make('category'),
-				FileUpload::make('thumbnail'),
-				TextArea::make('venue')
-					->autosize(),
-				DatePicker::make('date')
-					->closeOnDateSelection()
-					->native(false),
-				TimePicker::make('from')
-					->seconds(false)
-					->native(false),
-				TimePicker::make('to')
-					->seconds(false)
-					->native(false),
-				MarkdownEditor::make('about'),
-			]);
-	}
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('New Event')
+                    ->schema([
+                        FileUpload::make('thumbnail')
+                            ->image()
+                            ->maxFiles(1)
+                            ->columnSpanFull(),
+                        TextInput::make('name'),
+                        TextInput::make('category'),
+                        RichEditor::make('venue'),
+                        MarkdownEditor::make('about'),
+                        DatePicker::make('date')
+                            ->columnSpanFull(),
+                        TimePicker::make('from'),
+                        TimePicker::make('to'),
+                    ])->columns(2),
+            ]);
+    }
 
-	public static function table(Table $table): Table {
-		return $table
-			->columns([
-				//
-			])
-			->filters([
-				//
-			])
-			->actions([
-				Tables\Actions\EditAction::make(),
-			])
-			->bulkActions([
-				Tables\Actions\BulkActionGroup::make([
-					Tables\Actions\DeleteBulkAction::make(),
-				]),
-			])
-			->emptyStateActions([
-				Tables\Actions\CreateAction::make(),
-			]);
-	}
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
 
-	public static function getRelations(): array {
-		return [
-			//
-		];
-	}
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
 
-	public static function getPages(): array {
-		return [
-			'index' => Pages\ListEvents::route('/'),
-			'create' => Pages\CreateEvent::route('/create'),
-			'edit' => Pages\EditEvent::route('/{record}/edit'),
-		];
-	}
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvent::route('/create'),
+            'edit' => Pages\EditEvent::route('/{record}/edit'),
+        ];
+    }
 }
