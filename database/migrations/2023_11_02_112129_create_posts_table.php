e6a82fd7ed71b5c2a17faa06d3394f606bea585e
Ivan on Linux <admin@codprez.com>
2024-08-27 15:21:23 +0300

    uncomplete work

--- database/migrations/2023_11_02_112129_create_posts_table.php
+++ database/migrations/2023_11_02_112129_create_posts_table.php
@@ -7,28 +7,31 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	/**
-	 * Run the migrations.
-	 */
-	public function up(): void {
-		Schema::create('posts', function (Blueprint $table) {
-			$table->id();
-			$table->string('title');
-			$table->string('slug');
-			$table->boolean('isPublished')->default(false);
-			$table->string('thumbnail');
-			$table->string('tags');
-			$table->foreignIdFor(User::class)->constrained('users');
-			$table->text('body');
-			$table->foreignIdFor(Category::class)->constrained('categories');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->boolean('isPublished')->default(false);
+            $table->string('thumbnail');
+            $table->string('tags');
+            $table->foreignIdFor(User::class)->constrained('users');
+            $table->text('content');
+            $table->tinyText('excerpt')->nullable();
+            $table->foreignIdFor(Category::class)->constrained('categories');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 */
-	public function down(): void {
-		Schema::dropIfExists('posts');
-	}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
 };
--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    {{-- <title>{{ $title ?? '' }} - {{ getName() }}</title> --}}
     <title>@yield('title') - {{ getName() }}</title>
     <!-- Fav Icon -->
     <link rel="icon" href="{{ getFavIconUrl() }}" type="image/x-icon">
@@ -45,7 +44,7 @@
 <body>
     <div class="boxed_wrapper">
         <!-- preloader -->
-        {{-- <x-preloader /> --}}
+        <x-preloader />
         <!-- preloader end -->
 
         <!-- main header -->
